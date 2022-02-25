1216bd11e60cb95035a37d48232540dd028020a3
Dairo Parra <parra.dairo@gmail.com>
2022-02-25 02:48:25 -0500

    fix: changing types

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Product extends Model
 {
@@ -14,27 +18,27 @@ class Product extends Model
 
     protected $guarded = ['id', 'created_at', 'update_at'];
 
-    public function sizes()
+    public function sizes(): HasMany
     {
         return $this->hasMany(Size::class);
     }
 
-    public function brands(): Brand
+    public function brands(): BelongsTo
     {
         return $this->belongsTo(Brand::class);
     }
 
-    public function subcategory(): SubCategory
+    public function subcategory(): BelongsTo
     {
         return $this->belongsTo(SubCategory::class);
     }
 
-    public function colors()
+    public function colors(): BelongsToMany
     {
         return $this->belongsToMany(Color::class);
     }
 
-    public function images()
+    public function images(): MorphMany
     {
         return $this->morphMany(Image::class, "imageable");
     }
