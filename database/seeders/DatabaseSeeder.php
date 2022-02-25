89471fdfa0459ed69a2c6d7476ed93a7cb0d85f3
Dairo Parra <parra.dairo@gmail.com>
2022-02-25 11:22:35 -0500

    feature: Size seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,6 +29,7 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductSeeder::class);
         $this->call(ColorSeeder::class);
         $this->call(ColorProductSeeder::class);
+        $this->call(SizeSeeder::class);
 
     }
 }
