76463930f2a48096651a983eda887e3963cd2333
Dairo Parra <parra.dairo@gmail.com>
2022-02-24 23:21:19 -0500

    feat: relationship added

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Brand extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
